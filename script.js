3ce0fa418b38c4ab25c99f6c63b031a0af99f91d
Imdadul Ahmed <ahmedimdad1593@gmail.com>
2023-09-21 22:13:48 +0600

    chat-client-frontend-done

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 'use strict';
 
+var quotes = [
+  "I don't know the question, but sex is definitely the answer. - Woody Allen.",
+  'Everything in the world is about sex except sex. Sex is about power. - Oscar Wilde',
+  'Fighting for peace is like screwing for virginity. - George Carlin',
+  'If you want to get laid, go to college. If you want an education, go to the library.',
+  'The difference between sex and love is that sex relieves tension and love causes it. - Woody Allen',
+  'The main reason Santa is so jolly is because he knows where all the bad girls live - George Carlin',
+  'Sex without love is as hollow and ridiculous as love without sex. - Hunter S. Thompson',
+  "Sex is the consolation you have when you can't have love. - Gabriel garcia Marquez",
+  'No woman gets an orgasm from shining the kitchen floor - Betty Friedan',
+  'Sex is kicking death in the ass while singing - Charles Bukowski',
+  "Is sex dirty? Only when it's being done right - Woody Allen",
+  'Sex is an emotion in motion - Mae West',
+  'Sex is part of nature. I go with nature - Marilyn Monroe',
+  'A liberated woman is one who had sex before marriage, and a job after - Gloria Steinem',
+  "Sex without love is a meaningless experience, but as far as meaningless experiences go it's pretty damn good - Woody Allen",
+  'In my sex fantasy, nobody ever loves me for my mind - Nora Ephron',
+  "I'm a heroine addict. I need to have sex with women who have saved someone's life - Mitch Hedberg",
+  'Sex is not the answer. Sex is the question. "Yes" is the answer - Swami X',
+  'Physics is like sex: sure, it may give some practical results, but that is not why we do it - Richard Feynman',
+  'Sex is the most fun you can have without laughing - Woody Allen',
+  'Love is the answer, but while you are waiting for the answer, sex raises some pretty good questions - Woody Allen',
+  'Dancing is a perpendicular expression of a horizontal desire - George Bernard Shaw',
+  "Hate to sound sleazy, but tease me, I don't want it if it's that easy - Tupac Shakur",
+  "Things you never hear: Please stop sucking my dick or I'll call the police! - George Carlin",
+  'She was pleased to have him come and never sorry to see him go - Dorothy Parker',
+  'Sometimes I get real lonely sleeping with you - Haruki Murakami',
+  'You know that look that women get when they want to have sex? Me neither - Steve Martin',
+  "I'm such a good lover because I practice a lot on my own - Woody Allen",
+  'Women fake orgasms and men fake finances - Suze Orman',
+  'Good girls go to heaven and bad girls go everywhere - Helen Gurley Brown',
+  'Lift your hips for me, love - Tahereh Mafi',
+  'jan was an excellent fuck...she had a tight pussy and she took it like it was a knife that was killing her - Charles Bukowski',
+  'Don’t put off till tomorrow anyone you could be doing today - Emma Chase',
+  "A woman can become a man's friend only in the following stages - first an acquantaince, next a mistress, and only then a friend. - Anton Checkov",
+  'She was made for untidy rooms and rumpled beds - Alexander McCall Smith',
+  'Sex: the thing that takes up the least amount of time and causes the most amount of trouble. - John Barrymore',
+];
+
 // Wrap function definitions in jQuery ready callback to avoid defining them globally
 // (otherwise browser extensions may call them)
 $(() => {
@@ -71,14 +110,19 @@ $(() => {
 
   function simulateIncomingMessages() {
     setTimeout(() => {
-      $.get('https://api.quotable.io/random', (data) => {
-        const msg = new Message(data.content, false, Date.now());
-        // REMOVE-START
-        postMessage(msg);
-        // REMOVE-END
-        showMessage(msg);
-        scrollToBottom();
-      });
+      // $.get('https://api.quotable.io/random', (data) => {
+      //   const msg = new Message(data.content, false, Date.now());
+      //   // REMOVE-START
+      //   postMessage(msg);
+      // REMOVE-END
+      const msg = new Message(
+        quotes[Math.floor(Math.random() * quotes.length)],
+        false,
+        Date.now()
+      );
+      showMessage(msg);
+      scrollToBottom();
+      // });
     }, genRandomMs());
   }
 
@@ -90,7 +134,7 @@ $(() => {
   }
 
   // REMOVE-START
-  getMessages();
+  // getMessages();
   scrollToBottom();
   // REMOVE-END
   $('#msg-form').on('submit', (e) => {
@@ -100,7 +144,7 @@ $(() => {
       $('#text').val('');
       const msg = new Message(content, true, Date.now());
       // REMOVE-START
-      postMessage(msg);
+      // postMessage(msg);
       // REMOVE-END
       showMessage(msg);
       scrollToBottom();
